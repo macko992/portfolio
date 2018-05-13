dfdf87161d0ec91c19b5dc94af076749059cf9e0
macko992 <m4ck0992@gmail.com>
2018-05-13 15:21:07 +0200

    Filled section: contact, project and skills(60%)[Need to repair skills bar]

--- js/main.js
+++ js/main.js
@@ -1,25 +1,34 @@
 // slow slide
-  $(document).ready(function() {
-    $("a").on('click', function(event) {
-      if (this.hash !== "") {
-        event.preventDefault();
-        var hash = this.hash;
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top
-        }, 1500, function() {
-          window.location.hash = hash;
-        });
-      }
-    });
+$(document).ready(function() {
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 1500, function() {
+        window.location.hash = hash;
+      });
+    }
   });
+});
 
 //active and focous link
 var header = document.getElementById("navbar");
 var btn = header.getElementsByClassName("li");
-for (var i=0; i <btn.length; i++){
-  btn[i].addEventListener("click", function(){
+for (var i = 0; i < btn.length; i++) {
+  btn[i].addEventListener("click", function() {
     var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active","");
-    this.className +=" active";
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
   });
 }
+
+//skills bar
+$(document).ready(function() {
+  $('.progress .progress-bar').css("width",
+    function() {
+      return $(this).attr("aria-valuenow") + "%";
+    }
+  )
+});
